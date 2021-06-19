0cab4daaca98674b601055594ee9953eb41460eb
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2021-06-19 20:52:12 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/quoto/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quoto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,153 @@
+import 'dart:convert';
+import 'dart:math';
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+
+List jsonBackup = [];
+
+Future<List> loadQuotes() async {
+  var jsonText = await rootBundle.loadString('assets/quotes.json');
+  var data = json.decode(jsonText);
+  jsonBackup = data;
+  return data;
+}
+
+//
+// Future getQuote() async {
+// This example uses the Google Books API to search for books about http.
+// https://developers.google.com/books/docs/overview
+// var url = Uri.https('type.fit', '/api/quotes');
+//
+// Await the http get response, then decode the json-formatted response.
+// var response = await http.get(url);
+// if (response.statusCode == 200) {
+// var jsonResponse = convert.jsonDecode(response.body) as List<dynamic>;
+// var dataquote = jsonResponse[0];
+// var quote = dataquote['text'];
+// var author = dataquote['author'];
+// var data = [quote, author];
+// return data;
+// } else {
+// return ['Couldn\'t get Quotes ☹.', ''];
+// }
+// }
+//
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.f
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: SizedBox(
+            child: Icon(
+              Icons.format_quote,
+              size: 40,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: Quote(),
+      ),
+    );
+  }
+}
+
+class _QuoteState extends State<Quote> {
+  List<dynamic> _quotes = [];
+  dynamic _quote;
+  _randomItem(list) {
+    final random = new Random();
+    var i = random.nextInt(list.length);
+    return list[i];
+  }
+
+  _getQuote() async {
+    loadQuotes().then((val) => setState(() {
+          _quotes = val;
+          _quote = _randomItem(_quotes);
+        }));
+  }
+
+  // _QuoteState() {
+  //   _getQuote();
+  // }
+
+  @override
+  void initState() {
+    super.initState();
+    _getQuote();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: EdgeInsets.all(32.0),
+      child: Center(
+        child:
+            new Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          new Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text(
+              (_quote['text']),
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.black87,
+                  fontFamily: "Heveltica"),
+              textAlign: TextAlign.left,
+            ),
+            Text(
+              ("~ " + (_quote['author'])),
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.pink,
+                  fontFamily: "Heveltica",
+                  fontWeight: FontWeight.w700),
+              textAlign: TextAlign.center,
+              maxLines: 1,
+            ),
+          ]),
+          Container(
+              margin: EdgeInsets.only(top: 64.0),
+              child: IconButton(
+                  tooltip: "Refresh",
+                  onPressed: () {
+                    print("refreshed!");
+                    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("Quotes Refreshed Successfully!"),
+                      ),
+                    );
+                    setState(() {
+                      _quote = _randomItem(_quotes);
+                    });
+                  },
+                  splashColor: Colors.red,
+                  hoverColor: Colors.blueAccent[100],
+                  iconSize: 80,
+                  icon: Icon(
+                    Icons.all_inclusive_outlined,
+                    color: Colors.blue,
+                  )))
+        ]),
+      ),
+    );
+  }
+}
+
+class Quote extends StatefulWidget {
+  @override
+  _QuoteState createState() => _QuoteState();
+}
+
+void refresh() {}
